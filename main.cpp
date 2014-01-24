c63afcdd6c9764f6a294ee5b01e355c40fa5c3d4
Marcin Rainka <marcin.rainka@gmail.com>
2014-01-24 23:13:07 +0100

    Testing code.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "RandomGenerator.hpp"
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
@@ -15,9 +16,29 @@ using namespace std;
 int main( int argc, char** argv ) {
     RandomGenerator randomGenerator;
     
+    /* Random numbers of an integer type. */
+    cout << "Random integers <-10;10>: ";
     for ( int i = 0; i < 10; ++i ) {
-        cout << randomGenerator.RandomNumber( 0.0d, 1.0d ) << endl;
+        cout << randomGenerator.RandomNumber( -10, 10 ) << " ";
     }
+    cout << endl;
+    
+    /* Random numbers of an unsigned integer type. */
+    
+    unsigned int maxUnsignedInt = numeric_limits<unsigned int>::max();
+    
+    cout << "Random unsigned integers <" << ( maxUnsignedInt - 10 ) << ";" << maxUnsignedInt << ">: ";
+    for ( int i = 0; i < 10; ++i ) {
+        cout << randomGenerator.RandomNumber( ( maxUnsignedInt - 10 ), maxUnsignedInt ) << " ";
+    }
+    cout << endl;
+    
+    /* Random numbers of a double type. */
+    cout << "Random doubles (0;1): ";
+    for ( int i = 0; i < 10; ++i ) {
+        cout << randomGenerator.RandomNumber( 0.0d, 1.0d ) << " ";
+    }
+    cout << endl;
     
     return 0;
 }
