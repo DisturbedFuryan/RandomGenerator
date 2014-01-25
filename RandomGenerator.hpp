75566f45de3cf6efe1daaa732482cb5fceff5c6d
Marcin Rainka <marcin.rainka@gmail.com>
2014-01-25 18:03:01 +0100

    Possibility of creating random arrays.

--- RandomGenerator.hpp
+++ RandomGenerator.hpp
@@ -41,6 +41,34 @@ public:
     template< typename T >
     T RandomRealNumber( const T minimum = std::numeric_limits< T >::min(),
                         const T maximum = std::numeric_limits< T >::max() ) const;
+    
+    /**
+     * Creates an array with random integer numbers and returns pointer to the allocated array.
+     * IMPORTANT: You will have to deallocate array yourself by delete[] instruction!
+     * 
+     * @param  size     size of array
+     * @param  minimum  minimum value of random number
+     * @param  maximum  maximum value of random number
+     * @return          pointer to allocated array with random integer numbers
+     */
+    template< typename T >
+    T* CreateRandomIntegerArray( const unsigned int size,
+                                 const T minimum = std::numeric_limits< T >::min(),
+                                 const T maximum = std::numeric_limits< T >::max() ) const;
+                           
+    /**
+     * Creates an array with random real numbers and returns pointer to the allocated array.
+     * IMPORTANT: You will have to deallocate array yourself by delete[] instruction!
+     * 
+     * @param  size     size of array
+     * @param  minimum  minimum value of random number
+     * @param  maximum  maximum value of random number
+     * @return          pointer to allocated array with random real numbers
+     */
+    template< typename T >
+    T* CreateRandomRealArray( const unsigned int size,
+                              const T minimum = std::numeric_limits< T >::min(),
+                              const T maximum = std::numeric_limits< T >::max() ) const;
 
 
 private:
@@ -64,6 +92,46 @@ template< typename T >
 inline T RandomGenerator::RandomRealNumber( const T minimum, const T maximum ) const {
     std::uniform_real_distribution< T > uniformRealDist( minimum, maximum );
     return uniformRealDist( *m_defaultRandomEngine );
+}
+
+
+template< typename T >
+T* RandomGenerator::CreateRandomIntegerArray( const unsigned int size, const T minimum, const T maximum ) const {
+    T* randomIntegerArray = nullptr;
+    
+    if ( size > 0 ) {
+        /* Allocation. */
+        randomIntegerArray = new T[ size ];
+        
+        std::uniform_int_distribution< T > uniformIntDist( minimum, maximum );
+        
+        /* Fill the array with random numbers. */
+        for ( unsigned int u = 0; u < size; ++u ) {
+            randomIntegerArray[ u ] = uniformIntDist( *m_defaultRandomEngine );
+        }
+    }
+    
+    return randomIntegerArray;
+}
+
+
+template< typename T >
+T* RandomGenerator::CreateRandomRealArray( const unsigned int size, const T minimum, const T maximum ) const {
+    T* randomRealArray = nullptr;
+    
+    if ( size > 0 ) {
+        /* Allocation. */
+        randomRealArray = new T[ size ];
+        
+        std::uniform_real_distribution< T > uniformRealDist( minimum, maximum );
+        
+        /* Fill the array with random numbers. */
+        for ( unsigned int u = 0; u < size; ++u ) {
+            randomRealArray[ u ] = uniformRealDist( *m_defaultRandomEngine );
+        }
+    }
+    
+    return randomRealArray;
 }
 
 
