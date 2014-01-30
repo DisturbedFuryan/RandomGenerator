5e62e5efa35fb0d2c17d0fc824914ed8c5e1e3f4
Marcin Rainka <marcin.rainka@gmail.com>
2014-01-30 11:17:29 +0100

    Testing code for creating single numbers and arrays.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,20 @@
 using namespace std;
 
 
+template < typename T >
+void PrintArray( const T* array, const unsigned long long& size ) {
+    if ( size ) {
+        cout << "[ ";
+        
+        for ( unsigned long long i = 0; i < size; ++i ) {
+            cout << array[ i ] << " ";
+        }
+        
+        cout << "]\n";
+    }
+}
+
+
 //**********************************************************************************************************************
 /**
  * The main() function.
@@ -15,23 +29,72 @@ using namespace std;
  */
 int main( int argc, char** argv ) {
     // Creating a random integer generator, precisely - the random int type numbers generator.
-    RandomIntGenerator< int > randomIntGenerator;
+    RandomIntGenerator< int > randomIntsGenerator;
+    
+    // Creating a random real generator, precisely - the random double type numbers generator.
+    RandomRealGenerator< double > randomDoublesGenerator;
     
     // Print information about support.
     cout << "There is "
-         << ( randomIntGenerator.IsDeviceSupport() ? "device support, hooray!" : "no device support, sorry." ) << endl;
+         << ( randomIntsGenerator.IsDeviceSupport() ? "device support, hooray!" : "no device support, sorry." )
+         << endl << endl;
     
-    randomIntGenerator.Prepare( 1, 100 );
+    //------------------------------------------------------------------------------------------------------------------
+    // Let's see how it works for creating single numbers and arrays!
+    //------------------------------------------------------------------------------------------------------------------
     
-    int* array = randomIntGenerator.CreateNextArray( 10 );
+    // Getting single random number.
+    //------------------------------------------------------------------------------------------------------------------
     
-    cout << "[ ";
-    for ( unsigned i = 0; i < 10; ++i ) {
-        cout << array[ i ] << " ";
-    }
-    cout << "]\n";
+    cout << "Single random int type number within < 1; 100 >: " << randomIntsGenerator.GetOne( 1, 100) << endl;
+    cout << "Single random double type number within ( 0; 1 ): " << randomDoublesGenerator.GetOne( 0.0d, 1.0d)
+         << endl << endl;
     
-    delete[] array;
+    // Creating a single array filled with random numbers.
+    //------------------------------------------------------------------------------------------------------------------
+    
+    int* randomIntsArray = randomIntsGenerator.CreateOneArray( 10, 1, 100 );
+    double* randomDoublesArray = randomDoublesGenerator.CreateOneArray( 10, 0.0d, 1.0d );
+    
+    cout << "Single random array filled with random int type numbers within < 1; 100 >:\n";
+    PrintArray< int >( randomIntsArray, 10 );
+    cout << endl;
+    
+    cout << "Single random array filled with random double type numbers within < 0; 1 >:\n";
+    PrintArray< double >( randomDoublesArray, 10 );
+    cout << endl;
+    
+    delete[] randomIntsArray;
+    delete[] randomDoublesArray;
+    
+    // Changing a single number created before into random number.
+    //------------------------------------------------------------------------------------------------------------------
+    
+    int intNumber;
+    double doubleNumber;
+    
+    randomIntsGenerator.MakeOneRandom( intNumber, 1, 100 );
+    randomDoublesGenerator.MakeOneRandom( doubleNumber, 0.0d, 1.0d );
+    
+    cout << "Single int type number changed into random within < 1; 100 >: " << intNumber << endl;
+    cout << "Single double type number changed into random within ( 0; 1 ): " << doubleNumber << endl << endl;
+    
+    // Filling a single array created before with random numbers.
+    //------------------------------------------------------------------------------------------------------------------
+    
+    int intsArray[ 10 ];
+    double doublesArray[ 10 ];
+    
+    randomIntsGenerator.MakeOneArrayRandom( intsArray, 10, 1, 100 );
+    randomDoublesGenerator.MakeOneArrayRandom( doublesArray, 10, 0.0d, 1.0d );
+    
+    cout << "Single array created before and filled with random int type numbers within < 1; 100 >:\n";
+    PrintArray< int >( intsArray, 10 );
+    cout << endl;
+    
+    cout << "Single array created before and filled with random double type numbers within < 0; 1 >:\n";
+    PrintArray< double >( doublesArray, 10 );
+    cout << endl;
     
     return 0;
 }
